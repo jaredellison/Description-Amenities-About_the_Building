2238f960d327f644561568494b0ddb32357887be
Jared Ellison <jaredellison@gmail.com>
2019-01-02 11:49:30 -0500

    Use middleware for logging and change client application endpoint

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let {retriever} = require('./retrieverController.js')
+let { retriever } = require('./retrieverController.js');
+let morgan = require('morgan');
 
 let server = express();
 server.use(bodyParser.json());
-server.use(express.static(__dirname + '/client/dist'));
+server.use(morgan('dev'));
+server.use('/:id', express.static(__dirname + '/client/dist'));
 
 server.get('/streetBreezy/api/:id', retriever);
 
-let port = 3009
+let port = 3009;
 server.listen(port, () => {
-    console.log(`listning on port ${port}`)
-})
+  console.log(`listning on port ${port}`);
+});
 
 module.exports.server = server;
