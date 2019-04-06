b302240424eea83a90689a71fb9fc631f4ea567e
Jared Ellison <jaredellison@gmail.com>
2019-04-06 14:19:35 -0400

    Rewrite descriptionGenerator function to create more realistic mock descriptions

--- data.js
+++ data.js
@@ -1,68 +1,115 @@
-var dbComponents = require('./db.js')
+var dbComponents = require('./db.js');
 var faker = require('faker');
-let highlightAmenities = ['Cats and Dogs Allowed', 'Full-time Doorman', 'Washer/Dryer In-Unit', 'Elevator']
-let buildingAmenities = ['Bike Room', 'Green Building', 'Package Room', 'Children\'s playroom', 'Concierge', 'Laundry in Building', 'Smoke-free', 'Storage Available', 'Live-in Super', 'Cold-Storage', 'Gym', 'Virtual Doorman', 'Concierge', 'Media Room', 'Community Recreation Facility', 'Swimming Pool', 'Parking Available']
-let listingAmenities = ['Central Air-Conditioning', 'Dishwasher', 'Storage Available', 'Fireplace']
-let outdoorAmenities = ['Terrace', 'Roof Deck', 'Garden']
+let highlightAmenities = [
+  'Cats and Dogs Allowed',
+  'Full-time Doorman',
+  'Washer/Dryer In-Unit',
+  'Elevator'
+];
+let buildingAmenities = [
+  'Bike Room',
+  'Green Building',
+  'Package Room',
+  "Children's playroom",
+  'Concierge',
+  'Laundry in Building',
+  'Smoke-free',
+  'Storage Available',
+  'Live-in Super',
+  'Cold-Storage',
+  'Gym',
+  'Virtual Doorman',
+  'Concierge',
+  'Media Room',
+  'Community Recreation Facility',
+  'Swimming Pool',
+  'Parking Available'
+];
+let listingAmenities = [
+  'Central Air-Conditioning',
+  'Dishwasher',
+  'Storage Available',
+  'Fireplace'
+];
+let outdoorAmenities = ['Terrace', 'Roof Deck', 'Garden'];
 
 let highlightsCreator = (divider, HLAArray) => {
-    let length = parseInt((Math.random()*(HLAArray.length+1))/divider);
-    let newHLArray = [];
-    while(newHLArray.length < length){
-        let amenity = HLAArray[parseInt(Math.random()*HLAArray.length)];
-        if(!newHLArray.includes(amenity)){
-            newHLArray.push(amenity);
-        }
+  let length = parseInt((Math.random() * (HLAArray.length + 1)) / divider);
+  let newHLArray = [];
+  while (newHLArray.length < length) {
+    let amenity = HLAArray[parseInt(Math.random() * HLAArray.length)];
+    if (!newHLArray.includes(amenity)) {
+      newHLArray.push(amenity);
     }
-    // console.log('newHLArray',newHLArray);
-    return newHLArray
-}
+  }
+  // console.log('newHLArray',newHLArray);
+  return newHLArray;
+};
 
 let buildingAmensCreator = (divider, BAArray) => {
-    let length = parseInt((Math.random()*(BAArray.length+1))/divider);
-    let newBArray = [];
-    while(newBArray.length < length){
-        let amenity = BAArray[parseInt(Math.random()*BAArray.length)];
-        if(!newBArray.includes(amenity)){
-            newBArray.push(amenity);
-        }
+  let length = parseInt((Math.random() * (BAArray.length + 1)) / divider);
+  let newBArray = [];
+  while (newBArray.length < length) {
+    let amenity = BAArray[parseInt(Math.random() * BAArray.length)];
+    if (!newBArray.includes(amenity)) {
+      newBArray.push(amenity);
     }
-    //console.log(newBArray);
-    return newBArray
+  }
+  //console.log(newBArray);
+  return newBArray;
 };
 
 let listingAmensCreator = (divider, LAArray) => {
-    let length = parseInt((Math.random()*(LAArray.length+1))/divider);
-    let newLArray = [];
-    while(newLArray.length < length){
-        let amenity = LAArray[parseInt(Math.random()*LAArray.length)];
-        if(!newLArray.includes(amenity)){
-            newLArray.push(amenity);
-        }
+  let length = parseInt((Math.random() * (LAArray.length + 1)) / divider);
+  let newLArray = [];
+  while (newLArray.length < length) {
+    let amenity = LAArray[parseInt(Math.random() * LAArray.length)];
+    if (!newLArray.includes(amenity)) {
+      newLArray.push(amenity);
     }
-    //console.log(newLArray);
-    return newLArray;
+  }
+  //console.log(newLArray);
+  return newLArray;
 };
 
 let outdoorAmensCreator = (divider, OAArray) => {
-    let length = parseInt((Math.random()*(OAArray.length+1))/divider);
-    let newOArray = [];
-    while(newOArray.length < length){
-        let amenity = OAArray[parseInt(Math.random()*OAArray.length)];
-        if(!newOArray.includes(amenity)){
-            newOArray.push(amenity);
-        }
+  let length = parseInt((Math.random() * (OAArray.length + 1)) / divider);
+  let newOArray = [];
+  while (newOArray.length < length) {
+    let amenity = OAArray[parseInt(Math.random() * OAArray.length)];
+    if (!newOArray.includes(amenity)) {
+      newOArray.push(amenity);
     }
-    //console.log(newOArray);
-    return newOArray
+  }
+  //console.log(newOArray);
+  return newOArray;
 };
 
-let generator = () => {
-    return faker.lorem.words(num = 190);
-}
+var descriptionGenerator = () => {
+  let paragraphCount = Math.floor(9 * Math.random()) + 2;
+  return Array(paragraphCount)
+    .fill()
+    .map(() => {
+      let paragraphLength = Math.floor(9 * Math.random()) + 2;
+      return faker.lorem.paragraph(paragraphLength);
+    })
+    .join('\n \n');
+};
 
 dbComponents.db.once('open', function() {
   console.log('Saving to database');
-  dbComponents.saver(generator(), highlightsCreator, highlightAmenities, buildingAmensCreator, buildingAmenities, listingAmensCreator, listingAmenities, outdoorAmensCreator, outdoorAmenities);
+  dbComponents.saver(
+    descriptionGenerator(),
+    highlightsCreator,
+    highlightAmenities,
+    buildingAmensCreator,
+    buildingAmenities,
+    listingAmensCreator,
+    listingAmenities,
+    outdoorAmensCreator,
+    outdoorAmenities
+  );
 });
 
+faker.lorem.paragraph(4);
+console.log('hello');
