b159a3b6432e592e4e9f4a9f2923118d50a79603
Jared Ellison <jaredellison@gmail.com>
2019-04-08 10:11:10 -0400

    Add table-title css class for styling

--- client/table.jsx
+++ client/table.jsx
@@ -6,34 +6,34 @@ var Table = () => {
         <tbody>
             <tr id='tableRow0'>
                 <td className='tabletext' id='table0x0'>
-                    <span>DAYS ON MARKET</span>
+                    <span className='table-title'>DAYS ON MARKET</span>
                     <br></br>
                     <span>{parseInt((Math.random()*500))} Days</span>
                 </td>
                 <td className='tabletext' id='table1x0'>
-                    <span>LAST PRICE CHANGE</span>
+                    <span className='table-title'>LAST PRICE CHANGE</span>
                     <br></br>
                     <span>{parseInt((Math.random()*500))} Days Ago</span>
                 </td>
                 <td className='tabletext' id='table2x0'>
-                    <span>ESTIMATED PAYMENT</span>
+                    <span className='table-title'>ESTIMATED PAYMENT</span>
                     <br></br>
                     <span><a href=''>${parseInt((Math.random())*100000)}</a></span>
                 </td>
             </tr>
             <tr id='tableRow1'>
                 <td className='tabletext' id='table0x1'>
-                    <span>MONTHLY TAXES</span>
+                    <span className='table-title'>MONTHLY TAXES</span>
                     <br></br>
                     <span>${parseInt((Math.random())*7000)}</span>
                 </td>
                 <td className='tabletext' id='table1x1'>
-                    <span>MONTHLY MAINTENANCE</span>
+                    <span className='table-title'>MONTHLY MAINTENANCE</span>
                     <br></br>
                     <span>${parseInt((Math.random())*4000)}</span>
                 </td>
                 <td className='tabletext' id='table2x1'>
-                    <span>TAX ABATEMENT</span>
+                    <span className='table-title'>TAX ABATEMENT</span>
                     <br></br>
                     <span>No data available</span>
                 </td>
