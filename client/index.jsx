e76bde2632ad86f3a5aad7aaddd31eb1a939da66
Jared Ellison <jaredellison@gmail.com>
2019-04-08 10:14:34 -0400

    Use gradient and animated div height to reveal full description

--- client/index.jsx
+++ client/index.jsx
@@ -6,74 +6,89 @@ import HighlightAmens from './highlights.jsx';
 import BuildingAmens from './building.jsx';
 import ListingAmens from './listing.jsx';
 import OutdoorAmens from './outdoor.jsx';
-import showMore from './addMore.js';
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            fullDescriptionBox: undefined,
-            description: '',
-            descriptionPreSpan: '',
-            descriptionPostSpan: '',
-            highlightAmens: [],
-            buildingAmens: [],
-            listingAmens: [],
-            outdoorAmens: []
-        }
-        this.renderFunc = this.renderFunc.bind(this);
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullDescriptionBox: undefined,
+      description: '',
+      descriptionPreSpan: '',
+      descriptionPostSpan: '',
+      highlightAmens: [],
+      buildingAmens: [],
+      listingAmens: [],
+      outdoorAmens: [],
+      maximizeDescription: false
+    };
+    this.toggleDescription = this.toggleDescription.bind(this);
+  }
 
-    componentDidMount(){
-        // console.log('LOVE YOU KENNNYYYY')
-        this.renderFunc();
-    }
+  componentDidMount() {
+    Axios.get(`/api/description${window.location.pathname}`)
+      .then(response => {
+        let data = response.data;
+        this.setState({
+          fullDescriptionBox: data,
+          description: data.description,
+          highlightAmens: data.highlightAmens,
+          buildingAmens: data.buildingAmens,
+          listingAmens: data.listingAmens,
+          outdoorAmens: data.outdoorAmens
+        });
+      })
+      .catch(err => {
+        console.log(
+          `ERROR LOADING DESCRIPTION WITH ID ${window.location.pathname}`
+        );
+        console.log('THIS WAS THE ERR: ', err);
+      });
+  }
 
-    renderFunc(){
-        Axios.get(`/api/description${window.location.pathname}`)
-        .then((response) => {
-            let data = response.data;
-            this.setState({fullDescriptionBox: data,
-                description: data.description,
-                descriptionPreSpan: data.description.slice(0,300),
-                descriptionPostSpan: data.description.slice(300),
-                highlightAmens: data.highlightAmens,
-                buildingAmens: data.buildingAmens,
-                listingAmens: data.listingAmens,
-                outdoorAmens: data.outdoorAmens});
-        })
-        .catch((err) => {
-            console.log(`ERROR LOADING DESCRIPTION WITH ID ${window.location.pathname}`);
-            console.log('THIS WAS THE ERR: ', err);
-        })
-    }
+  toggleDescription() {
+    document
+      .getElementById('description')
+      .classList.toggle('minimize-description');
+    document
+      .getElementById('description')
+      .classList.toggle('maximize-description');
+    this.setState({ maximizeDescription: !this.state.maximizeDescription });
+  }
 
-    render(){
-        return(
-        <div id='entireDescriptionBox'>
-            <div>
-                <Table />
-            </div>
-            <div id='descriptionBox'>
-                <h5 id='descriptionHead'>Description </h5>
-                <p id='description'>{this.state.descriptionPreSpan}<span id='dots'>...</span><span id='moreText'>{this.state.descriptionPostSpan}</span></p>
-                <button onClick={() => {showMore()}} id='readMore'>Read More</button>
-            </div>
-            <hr className='separator'/>
-            <div>
-                <h5 id="amenitiesHead">Amenities</h5>
-                <h5 id='highlightAmens' class="amens">Highlights </h5>
-                <HighlightAmens highlights={this.state.highlightAmens}/>
-                <h5 class="amens">Building Amenities </h5>
-                <BuildingAmens buildings={this.state.buildingAmens}/>
-                <h5 class="amens">Listing Amenities </h5>
-                <ListingAmens listings={this.state.listingAmens}/>
-                <h5 class="amens">Outdoor Amenities </h5>
-                <OutdoorAmens outdoors={this.state.outdoorAmens}/>
-            </div>
+  render() {
+    return (
+      <div id="entireDescriptionBox">
+        <div>
+          <Table />
         </div>
-        )
-    }
+        <div id="descriptionBox">
+          <h5 id="descriptionHead">Description </h5>
+          <div id="description-text-container">
+            <p id="description" className="minimize-description">
+              {this.state.description}
+            </p>
+          </div>
+          <div onClick={this.toggleDescription} id="readMore">
+            {this.state.maximizeDescription ? 'Read Less' : 'Read More'}
+          </div>
+        </div>
+        <hr className="separator" />
+        <div>
+          <h5 id="amenitiesHead">Amenities</h5>
+          <h5 id="highlightAmens" class="amens">
+            Highlights{' '}
+          </h5>
+          <HighlightAmens highlights={this.state.highlightAmens} />
+          <h5 class="amens">Building Amenities </h5>
+          <BuildingAmens buildings={this.state.buildingAmens} />
+          <h5 class="amens">Listing Amenities </h5>
+          <ListingAmens listings={this.state.listingAmens} />
+          <h5 class="amens">Outdoor Amenities </h5>
+          <OutdoorAmens outdoors={this.state.outdoorAmens} />
+        </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('description-container'));
